c888269c7cfe20bb263f0f1244d4f919bdf6c03d
atomohod <atomohod3@gmail.com>
2016-05-13 15:41:14 +0300

    bubble sorting

---
+++ AlgorithmsAndDataStructures.Tests/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmsAndDataStructures.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void BubbleSort()
+        {
+            //arrange
+            var t1 = new[] { 3, 8, 6, 4 };
+            var t2 = new[] { 2, 8, 4, 6, 9, 9, 9, 0, 5, 3, 7, 4, 0, 1, 2, 2, 6, 2 };
+            var t3 = new[] { 1, 3, 4, 6, 7, 8, 9 };
+            var t4 = new[] { 9, 7, 7, 6, 5, 4, 3, 1 };
+            //act
+
+            BubbleSort bs = new BubbleSort();
+            bs.Sort(t1);
+            bs.Sort(t2);
+            bs.Sort(t3);
+            bs.Sort(t4);
+
+            //assert
+            Assert.IsTrue(IsGrowing(t1));
+            Assert.IsTrue(IsGrowing(t2));
+            Assert.IsTrue(IsGrowing(t3));
+            Assert.IsTrue(IsGrowing(t4));
+
+            Assert.IsTrue(AreEqual(t1, new[] {3, 4, 6, 8}));
+            Assert.IsTrue(AreEqual(t2, new[] {0, 0, 1, 2, 2, 2, 2, 3, 4, 4, 5, 6, 6, 7, 8, 9, 9, 9}));
+        }
+
+        private bool IsGrowing(int[] t)
+        {
+            for (int i = 1; i < t.Length; i++)
+            {
+                if (t[i] < t[i - 1])
+                    return false;
+            }
+
+            return true;
+        }
+
+        private bool AreEqual(int[] a, int[] b)
+        {
+            if (a.Length != b.Length)
+                return false;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != b[i])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsAndDataStructures
+{
+    public class BubbleSort
+    {
+        public void Sort(int[] input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = i; j < input.Length; j++)
+                {
+                    if (input[i] > input[j])
+                    {
+                        var t = input[i];
+                        input[i] = input[j];
+                        input[j] = t;
+                    }
+                }
+            }
+        }
+    }
+}
